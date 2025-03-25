9a27e7084a2c01ba57878db7fe213a4dd18b5924
Andrey Sadkov <ad.sadkov@gmail.com>
2025-03-25 17:35:28 +0300

    feat(config): added configuration loading method
    - Implemented LoadConfig method to load configuration from a file.
    - Utilized the cleanenv library for reading configuration.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"fmt"
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+	"sync"
+)
+
 type (
 	Config struct {
 		Application ApplicationConfig `yaml:"application"`
@@ -34,3 +41,30 @@ type (
 		KeyFile  string `yaml:"key_file"`
 	}
 )
+
+var once sync.Once
+
+func LoadConfig() (*Config, error) {
+	var config *Config
+	var err error
+
+	once.Do(func() {
+		configPath := os.Getenv("CONFIG_PATH")
+		if configPath == "" {
+			err = fmt.Errorf("environment variable $CONFIG_PATH is not set")
+			return
+		}
+
+		if _, err = os.Stat(configPath); os.IsNotExist(err) {
+			err = fmt.Errorf("config file %s does not exist", configPath)
+			return
+		}
+
+		if err = cleanenv.ReadConfig(configPath, &config); err != nil {
+			err = fmt.Errorf("failed to read config file: %w", err)
+			return
+		}
+	})
+
+	return config, err
+}
