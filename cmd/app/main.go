7f20678bab4a4c65bd36eb7ae46c1cda6e2921d1
Andrey Sadkov <ad.sadkov@gmail.com>
2025-03-26 10:31:00 +0300

    feat(app): added start application via TLS
    - Added Start method to start the application.
    - Handling of termination signals (SIGINT, SIGTERM) is implemented.
    - Echo server is started via TLS.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/rs/zerolog/log"
+	"home-library/internal/app"
 	"home-library/pkg/config"
 	"time"
 	_ "time/tzdata"
@@ -16,5 +17,13 @@ func main() {
 	_, err = time.LoadLocation(cfg.Application.TimeZone)
 	if err != nil {
 		log.Fatal().Err(err).Msgf("failed to load timezone")
+	}
+
+	application, err := app.NewApp(*cfg)
+	if err != nil {
+		log.Fatal().Err(err).Msgf("failed to create application")
+	}
+	if err := application.Start(); err != nil {
+		log.Fatal().Err(err).Msgf("failed to start application")
 	}
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,11 +1,18 @@
 package app
 
 import (
+	"context"
+	"fmt"
 	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog/log"
 	"gorm.io/gorm"
 	"home-library/internal/server"
 	"home-library/pkg/config"
 	"home-library/pkg/storage"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 type App struct {
@@ -25,6 +32,38 @@ func NewApp(cfg config.Config) (*App, error) {
 		echo: server.NewEchoServer(&cfg.HTTPServer),
 		db:   db,
 	}, nil
+}
+
+func (app *App) Start() error {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-quit
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(app.cfg.Application.ShutdownTimeout)*time.Second)
+		defer cancel()
+
+		if err := app.echo.Shutdown(ctx); err != nil {
+			log.Error().Err(err).Msg("failed to shutdown server")
+		} else {
+			log.Info().Msg("server was successfully shutdown")
+		}
+
+		err := app.closeDatabaseConnection()
+		if err != nil {
+			log.Error().Err(err).Msg("failed to close database connection")
+		} else {
+			log.Info().Msg("database connection was successfully closed")
+		}
+	}()
+
+	address := fmt.Sprintf("%s:%d", app.cfg.HTTPServer.Host, app.cfg.HTTPServer.Port)
+	if err := app.echo.StartTLS(address, app.cfg.SSL.CertFile, app.cfg.SSL.KeyFile); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (app *App) closeDatabaseConnection() error {
