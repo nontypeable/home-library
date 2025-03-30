aff7b19396c4e0e3aed331dbbbc46592a6ef1e47
Andrey Sadkov <ad.sadkov@gmail.com>
2025-03-30 13:58:16 +0300

    fix(user): fixed DeletedAt field definition for proper SQL query execution
    - Changed DeletedAt type from time.Time to *time.Time to support nullable values.

--- internal/services/user/entities/user.go
+++ internal/services/user/entities/user.go
@@ -14,17 +14,17 @@ const (
 )
 
 type User struct {
-	UserID      uuid.UUID `db:"user_id"`
-	FirstName   string    `db:"first_name"`
-	LastName    string    `db:"last_name"`
-	Email       string    `db:"email"`
-	PhoneNumber string    `db:"phone_number"`
-	Password    string    `db:"password"`
-	UserType    UserType  `db:"user_type"`
-	IsActive    bool      `db:"is_active"`
-	CreatedAt   time.Time `db:"created_at"`
-	UpdatedAt   time.Time `db:"updated_at"`
-	DeletedAt   time.Time `db:"deleted_at,omitempty"`
+	UserID      uuid.UUID  `db:"user_id"`
+	FirstName   string     `db:"first_name"`
+	LastName    string     `db:"last_name"`
+	Email       string     `db:"email"`
+	PhoneNumber string     `db:"phone_number"`
+	Password    string     `db:"password"`
+	UserType    UserType   `db:"user_type"`
+	IsActive    bool       `db:"is_active"`
+	CreatedAt   time.Time  `db:"created_at"`
+	UpdatedAt   time.Time  `db:"updated_at"`
+	DeletedAt   *time.Time `db:"deleted_at,omitempty"`
 }
 
 func NewUser() *User {
